1cf8c460adbdfbeca02e12cd45849946e4faea7a
morarodgers <morarodgers@gmail.com>
2023-06-17 22:03:16 +0300

    Added Edit buto to edit messages

--- js/index.js
+++ js/index.js
@@ -6,12 +6,13 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 
 const copyright = document.createElement('p');
-copyright.textContent = 'Rodgers \u00A9' + thisYear;
+copyright.textContent = 'Rodgers Mora Onchanah \u00A9' + thisYear;
 copyright.style.color = 'turquoise';
+
 footer.appendChild(copyright);
 
 // Array with skills
-const skills = ["JavaScript", "Python", "A little bit of C++"];
+const skills = ["HTML", "CSS", "JavaScript", "Python", "A little bit of C++"];
 
 const skillsSection = document.querySelector('#skills');
 const skillslist = skillsSection.querySelector('ul');
@@ -31,14 +32,9 @@ messageForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Get values from the controls
-    const usersName = event.target.usersName.value;
-    const usersEmail = event.target.usersEmail.value;
+    const userName = event.target.usersName.value;
+    const userEmail = event.target.usersEmail.value;
     const message = event.target.usersMessage.value;
-
-    // Log the user enetered values to the console
-    console.log("User's Name: ", usersName);
-    console.log("User's Email: ", usersEmail);
-    console.log("Message: ", message);
 
     // Clear form after submitting
     messageForm.reset();
@@ -52,7 +48,18 @@ messageForm.addEventListener('submit', function(event) {
     // Add the list items
     const messageList = messageSection.querySelector('ul');
     const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<a href='mailto:${usersEmail}'>${usersName}</a> <span>${message}</span>`;
+    newMessage.innerHTML = `<a href='mailto:${userEmail}'>${userName}</a> <span>${message}</span>`;
+    
+    // Create a a container for the input
+    const messageText = document.createElement('span');
+    messageText.classList.add('messageTxt');
+    messageText.textContent = message;
+
+    // Create input for editing messages
+    const editInput = document.createElement('input');
+    editInput.classList.add('edit-input');
+    editInput.type = 'text';
+    
 
     // Create the remove button
     const removeButton = document.createElement('button');
@@ -69,15 +76,41 @@ messageForm.addEventListener('submit', function(event) {
           messageSection.style.display = 'block';
         }
     });
-    // Create edit button
+
+    // Create edit button and a Function to run when it is clicked
     const editButton = document.createElement('button');
     editButton.innerText = 'edit';
     editButton.type = 'button';
-    /*editButton.addEventListener('click', function() {
-    });*/
+    editButton.addEventListener('click', function () {
+      const entry = event.target.parentNode;
+      const messageText = entry.querySelector('.messageTxt');
+      const editInput = entry.querySelector('.edit-input');
+    
+      messageText.style.display = 'none';
+      editInput.value = message.textContent;
+      editInput.style.display = 'inline-block';
+      editInput.focus();
+    });
+
+    // Create save button and a Function to run when the button is clicked
+    const saveButton = document.createElement('button');
+    saveButton.innerText = 'save';
+    saveButton.type = 'button';
+    saveButton.addEventListener('click', function () {
+      const entry = event.target.parentNode;
+      const messageText = entry.querySelector('.messageTxt');
+      const editInput = entry.querySelector('.edit-input');
+      const editedMessage = editInput.value;
+    
+      messageText.textContent = editedMessage;
+      messageText.style.display = 'inline-block';
+      editInput.style.display = 'none';
+    });
+    
 
 newMessage.appendChild(removeButton);
 newMessage.appendChild(editButton);
+newMessage.appendChild(saveButton);
 messageList.appendChild(newMessage);
 });
 
